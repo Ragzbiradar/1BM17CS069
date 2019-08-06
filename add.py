13813f370fb52d052ae3dd79ab58ab49068b41b7
Ragzbiradar <53818125+Ragzbiradar@users.noreply.github.com>
2019-08-06 15:34:14 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,19 @@
+
+
+import sys
+import math
+n= int(input("enter n"))
+if n==0:
+    print(n,"is zero")
+elif n <0 :
+    print(n," is negative")
+else :
+    print(n," is positive")
+    
+
+
+ 
+
+
+
+
