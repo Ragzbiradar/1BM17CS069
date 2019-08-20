323b486e4a02150b4fe49bdad54b06a91e5b5386
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-08-20 15:03:41 +0530

    linear search pp lab 2a

---
+++ pplab2a.py
@@ -0,0 +1,20 @@
+def search(lis,a):
+    print(lis)
+    if a in lis:
+        return True
+    else:
+        return False
+
+lis=[]
+while True:
+    a=int(input("enter the element ....-1 to stop"))
+    if a!= -1:
+        lis.append(a)
+    else :
+        break
+b=int(input("enter the element to be searched"))
+brr=search(lis,b)
+if brr:
+    print(str(b)+"found")
+else:
+    print("not found")
