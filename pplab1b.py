dc1755e261a3a76498157858e92e715f90047da4
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-08-20 15:02:56 +0530

    fibonacci pp lab 1b

---
+++ pplab1b.py
@@ -0,0 +1,14 @@
+def fibo(n):
+    x=0
+    y=1
+    i=0
+    
+    while i<a:
+         print(str(x)+"," ,end=" ")
+         x,y=y,x+y
+         i+=1
+    
+
+a=int(input("enter length of series :"))
+fibo(a)
+
