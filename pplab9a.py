ce7d982dff7db27903f2f04c3fa1265c12124124
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-11-12 15:43:36 +0530

    Update pplab9a.py

--- pplab9a.py
+++ pplab9a.py
@@ -4,30 +4,35 @@ class Application(Frame):
 	def __init__(self,master):
 		Frame.__init__(self,master)
 		self.grid()
+		self.m1=" "
+		self.m2=""
+		self.m3=""
 		self.create_widgets()
 
+
+
 	def create_widgets(self):
-		Label(self,text="Movie Selection").grid(row=0,column=0,columnspan=2,sticky=W)
+		Label(self,text="M)VIE BOOKING").grid(row=0,column=0,columnspan=2,sticky=W)
 		self.language = StringVar()
 		Label(self,text="Choose a Language").grid(row=1,column=0,columnspan=2,sticky=W)
-		Radiobutton(self,value="English",variable=self.language,text="English").grid(row=2,column=0,sticky=W)
-		Radiobutton(self,value="Kannada",variable=self.language,text="Kannada").grid(row=2,column=1,sticky=W)
-		Radiobutton(self,value="Hindi",variable=self.language,text="Hindi").grid(row=2,column=2,sticky=W)
-		self.movieselect1 = BooleanVar()
-		self.movieselect2 = BooleanVar()
-		self.movieselect3 = BooleanVar()
-		Label(self,text="Choose Movies:").grid(row=3,column=0,columnspan=2,sticky=W)
-		Checkbutton(self,variable= self.movieselect1,text="Titanic").grid(row=4,column=0,sticky=W)
-		Checkbutton(self,variable= self.movieselect2,text="Kirik Party").grid(row=4,column=1,sticky=W)
-		Checkbutton(self,variable= self.movieselect3,text="Chicchore").grid(row=4,column=2,sticky=W)
-		Label(self,text="Select the Number of Tickets:").grid(row=5,column=0,columnspan=2,sticky=W)
+		Radiobutton(self,value="English",variable=self.language,text="English",command=self.select).grid(row=2,column=0,sticky=W)
+		Radiobutton(self,value="Kannada",variable=self.language,text="Kannada",command=self.select).grid(row=3,column=0,sticky=W)
+		Radiobutton(self,value="Hindi",variable=self.language,text="Hindi",command=self.select).grid(row=4,column=0,sticky=W)
+		# self.movieselect1 = BooleanVar()
+		# self.movieselect2 = BooleanVar()
+		# self.movieselect3 = BooleanVar()
+		# Label(self,text="Choose Movies:").grid(row=5,column=0,columnspan=2,sticky=W)
+		# Checkbutton(self,variable= self.movieselect1,text="joker").grid(row=6,column=0,sticky=W)
+		# Checkbutton(self,variable= self.movieselect2,text="bell bottom").grid(row=7,column=0,sticky=W)
+		# Checkbutton(self,variable= self.movieselect3,text="Chicchore").grid(row=8,column=0,sticky=W)
+		Label(self,text="Select the Number of Tickets:").grid(row=9,column=0,columnspan=2,sticky=W)
 		self.tickets = StringVar()
 		choices=['1','2','3','4','5']
 		self.tickets.set('1')
 		self.popupMenu = OptionMenu(self, self.tickets, *choices)
-		self.popupMenu.grid(row = 6, column =1,sticky=W)
+		self.popupMenu.grid(row = 10, column =1,sticky=W)
 		self.btn = Button(self,text="Submit",command=self.check)
-		self.btn.grid(row=10,column=2,sticky=W)
+		self.btn.grid(row=18,column=2,sticky=W)
 
 
 	def check(self):
@@ -38,6 +43,26 @@ class Application(Frame):
 		else:
 			messagebox.showinfo("Success","Successfull")
 	
+	def select(self):
+		self.movieselect1 = BooleanVar()
+		self.movieselect2 = BooleanVar()
+		self.movieselect3 = BooleanVar()
+		Label(self,text="Choose Movies:").grid(row=5,column=0,columnspan=2,sticky=W)
+		if self.language.get()=="English":
+			Checkbutton(self,variable= self.movieselect1,text="joker").grid(row=6,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect2,text="Endgame").grid(row=7,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect3,text="avengers").grid(row=8,column=0,sticky=W)
+		if self.language.get()=="Kannada":
+			Checkbutton(self,variable= self.movieselect1,text="KGF").grid(row=6,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect2,text="bell bottom").grid(row=7,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect3,text="Kirik party").grid(row=8,column=0,sticky=W)
+		if self.language.get()=="Hindi":
+			Checkbutton(self,variable= self.movieselect1,text="War").grid(row=6,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect2,text="bala").grid(row=7,column=0,sticky=W)
+			Checkbutton(self,variable= self.movieselect3,text="Chicchore").grid(row=8,column=0,sticky=W)
+			
+
+
 
 root = Tk()
 root.title("Movie ticket booking")
